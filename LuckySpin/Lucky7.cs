ce65542683220527c03c935a39e9c700fe9fc08d
Brian Bansenauer <brianb@cascadia.edu>
2020-01-13 17:19:32 -0800

    commented Lucky7 class

--- LuckySpin/Lucky7.cs
+++ LuckySpin/Lucky7.cs
@@ -8,21 +8,23 @@ namespace LuckySpin
     public class Lucky7
     {
         public string Output() {
+            //Get three random values
                 Random random = new Random();
                 int a = random.Next(1, 10);
                 int b = random.Next(1, 10);
                 int c = random.Next(1, 10);
 
-                string output = 
+            //Use the ternary operator ?: to assign a value to the image variable based a random 7
+            string image = (a == 7 || b == 7 || c == 7) ?
+                    "<img src='http://faculty.cascadia.edu/brianb/images/LuckySevenExercise.jpg'/>" :
+                    ""; //empty string if none of the values are 7
+
+            //Create the output String using the static method Format
+            string output = String.Format(
                     "<button onclick='history.go(0)'>Spin</button>" +
-                    "<div>" + a + "</div><div>" + b + "</div><div>" + c + "</div>" +
-                    "<h1> Lucky Seven </h1>";
-                if (a == 7 || b == 7 || c == 7)
-                    output = output + 
-                        "<img src='http://faculty.cascadia.edu/brianb/images/LuckySevenExercise.jpg'/>";
-
-
-                return output;
+                    "<div>{0}</div><div>{1}</div><div>{2}</div>" +
+                    "<h1> Lucky Seven </h1>{3}", a, b, c, image);
+            return output;
         }
     }
 }
