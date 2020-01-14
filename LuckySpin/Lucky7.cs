ff56d1ba33e6a3747a525d31eed36349abab3fd2
Brian Bansenauer <brianb@cascadia.edu>
2020-01-14 08:48:33 -0800

    updates TODOs

--- LuckySpin/Lucky7.cs
+++ LuckySpin/Lucky7.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace LuckySpin
 {
-    public class Lucky7
+    public interface LuckySpin
     {
+        string Output(int luck);
+    }
+
+    public class Lucky7 
+    {
+        //TODO: Add an integer parameter to the Output method called "luck"
         public string Output() {
             //Get three random values
                 Random random = new Random();
@@ -14,10 +20,11 @@ namespace LuckySpin
                 int b = random.Next(1, 10);
                 int c = random.Next(1, 10);
 
-            //Use the ternary operator ?: to assign a value to the image variable based a random 7
+            //Use the ternary operator ?: to assign a value to the image variable
+            //TODO: Use the parameter value "luck" instead of the fixed number 7
             string image = (a == 7 || b == 7 || c == 7) ?
                     "<img src='http://faculty.cascadia.edu/brianb/images/LuckySevenExercise.jpg'/>" :
-                    ""; //empty string if none of the values are 7
+                    ""; //empty string if none of the values match
 
             //Create the output String using the static method Format
             string output = String.Format(
--- LuckySpin/Startup.cs
+++ LuckySpin/Startup.cs
@@ -15,16 +15,13 @@ namespace LuckySpin
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<Lucky7>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Lucky7 lucky7)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync(lucky7.Output());
-            });
+            //TODO: Include the UseRouting and UseEndpoints Middleware to map the Controller Route (see slides)
         }
     }
 }
